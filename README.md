09e40cb20cdd5efb85a20d8f2b8e10d92230af53
EcliS-e <marianita345@icloud.com>
2024-11-11 17:37:26 -0500

    code so far

---
+++ README.md
@@ -0,0 +1,2 @@
+# HW4
+ the long game
---
+++ hw4/hw4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream aFile = new FileStream("username.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(aFile);
+
+            int playerScore = 0;
+
+            Console.WriteLine("What's your name?");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+
+
+                playerScore++;
+                Console.WriteLine("/n Your score is " + playerScore);
+                sw.WriteLine(playerScore);
+            }
+
+
+            Environment.Exit(0);
+
+            /*
+            When(keyPressed) {
+                playerScore++;
+
+                Console.WriteLine(playerScore);
+
+            }
+            If
+'*/
+
+
+
+
+        }
+    }
+}
