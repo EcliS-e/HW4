372a6ff83df396f1e0ac34394cc89864f51af72d
EcliS-e <marianita345@icloud.com>
2024-11-12 19:20:19 -0500

    Inc. file I/O and added comments

--- hw4/hw4/Program.cs
+++ hw4/hw4/Program.cs
@@ -15,43 +15,54 @@ namespace HW4
     internal class Program
     {
         static void Main(string[] args)
-        {
-            FileStream aFile = new FileStream("username.txt", FileMode.OpenOrCreate);
-            StreamWriter sw = new StreamWriter(aFile);
+        { 
 
-            int playerScore = 0;
+            //Initialize variables 
+            string userName;
+            int playerScore;
 
-            Console.WriteLine("What's your name?");
+            //Check if a previous file exists, if not, prompt the user to enter a username
+            //and start out with the score of zero.
+            if (File.Exists("username.txt"))
+            {
+                
+                using (StreamReader sr = new StreamReader("username.txt"))
+                {
+                   // load previous data into user name and player score variables 
+                    userName = sr.ReadLine();
+                    playerScore = Int32.Parse(sr.ReadLine());
 
-            // Create a string variable and get user input from the keyboard and store it in the variable
-            string userName = Console.ReadLine();
+                    Console.WriteLine("Welcome " + userName);
 
 
+                }
+            }
+            else
+            {
+                //prompt username and set playerscore to 0
+                Console.WriteLine("What's your name?");
 
+                userName = Console.ReadLine();
+                playerScore = 0;
+            }
+
+            //increase score while keys except enter are pressed
             while (Console.ReadKey().Key != ConsoleKey.Enter)
             {
 
 
                 playerScore++;
-                Console.WriteLine("/n Your score is " + playerScore);
+                Console.WriteLine("\n Your score is " + playerScore);
+
+            }
+            //Save data : username and player score  
+            using (StreamWriter sw = new StreamWriter("username.txt"))
+            {
+                sw.WriteLine(userName);
                 sw.WriteLine(playerScore);
             }
 
-
-            Environment.Exit(0);
-
-            /*
-            When(keyPressed) {
-                playerScore++;
-
-                Console.WriteLine(playerScore);
-
-            }
-            If
-'*/
-
-
-
+            
 
         }
     }
